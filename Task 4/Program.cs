1d2ba347cbd3625b9312719544b17eef133b8cbf
UsernameZuke <127862838+UsernameZuke@users.noreply.github.com>
2023-03-15 15:23:53 +0700

    Add files via upload

---
+++ Task 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeWeeksInParadise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string ageWord;
+            string zodiacSign;
+            string placeOfWork;
+            Console.Write("Введите ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите ваш возвраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ваш знак зодиака: ");
+            zodiacSign = Console.ReadLine();
+            Console.Write("Введите ваше место работы: ");
+            placeOfWork = Console.ReadLine();
+            int lastNumberInAge = age % 10;
+            if (lastNumberInAge == 1)
+            {
+                ageWord = "год";
+            }
+            else if (lastNumberInAge > 1 && lastNumberInAge<5)
+            {
+                ageWord = "года";
+            }
+            else
+            {
+                ageWord = "лет";
+            }
+            Console.WriteLine($"Вас зовут {name}, вам {age} {ageWord}. Ваш знак зодиака {zodiacSign}. Ваше место работы {placeOfWork}");
+        }
+    }
+}
